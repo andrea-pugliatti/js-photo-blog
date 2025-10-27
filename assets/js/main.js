e5fde98c290f662b65670895d603187c6e266d9e
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-27 11:28:32 +0100

    Add logic to fetch cards from api and print the cards on screen

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
+
+// Select row
+const boardElement = document.getElementById("board");
+
+// Show Card
+const showCard = (element, card) => {
+	const { id, title, date, url } = card;
+
+	const cardElement = document.createElement("div");
+	cardElement.className += "card";
+
+	const pinElement = document.createElement("div");
+	pinElement.className += "pin";
+	const pinImage = document.createElement("img");
+	pinImage.src = "./assets/img/pin.svg";
+	cardElement.appendChild(pinElement);
+
+	const imageContainerElement = document.createElement("div");
+	imageContainerElement.className += "image-container";
+	const imageElement = document.createElement("img");
+	imageElement.src = url;
+	imageContainerElement.appendChild(imageElement);
+	cardElement.appendChild(imageContainerElement);
+
+	const cardFooterElement = document.createElement("div");
+	cardFooterElement.className += "card-footer";
+	const idElement = document.createElement("div");
+	idElement.className += "card-id";
+	idElement.textContent = id;
+	cardFooterElement.appendChild(idElement);
+	const titleElement = document.createElement("div");
+	titleElement.className += "card-title";
+	titleElement.textContent = title;
+	cardFooterElement.appendChild(titleElement);
+	const dateElement = document.createElement("div");
+	dateElement.className += "card-date";
+	dateElement.textContent = date;
+	cardFooterElement.appendChild(dateElement);
+	cardElement.appendChild(cardFooterElement);
+
+	element.appendChild(cardElement);
+};
+
+const fetchCards = (endpoint) => {
+	// let cards = await fetch(endpoint).then((response) => {
+	// 	response.text();
+	// });
+	// cards.forEach((card) => {
+	// 	showCard(boardElement, card);
+	// });
+
+	fetch(endpoint)
+		.then((response) => response.json())
+		.then((result) => {
+			result.forEach((card) => {
+				console.log(card);
+				showCard(boardElement, card);
+			});
+		})
+		.catch((error) => console.error(error));
+};
+
+fetchCards(endpoint);
